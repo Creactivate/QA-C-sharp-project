5658bab5c4bfdd8df8190579454acc5728d4175b
L <lukehurlockphoto@gmail.com>
2021-07-09 15:10:05 +0100

    first commit

---
+++ DBInteraction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Base_Project_C_Sharp
+{
+    class DBInteraction
+    {
+        public DBInteraction()
+        {
+            // Constructor: setup database connection
+            Con = new MySqlConnection("server=localhost;user=root;password=root;database=nbs");
+            Con.Open();
+        }
+
+        private MySqlConnection Con;
+
+        private MySqlCommand cmd;
+
+        public void printTable(string query)
+        {
+            cmd = new MySqlCommand(query,Con);
+            MySqlDataReader data = cmd.ExecuteReader();
+            while (data.Read())
+            {
+                Console.WriteLine("headings");
+                Console.WriteLine("_____________________________________________");
+                for (int i = 0; i < data.FieldCount;i++)
+                {
+                    if (i == data.FieldCount - 1)
+                    {
+                        Console.WriteLine($"|  {data[i]}  |");
+                    }
+                    else
+                    {
+                        Console.Write($"|  {data[i]}  ");
+                    }
+                    Console.WriteLine("_____________________________________________");
+                }
+
+            }
+        }
+
+        public void insertIntoTable(string productName, float price, int qty)
+        {
+            //Insert values into table
+            cmd = new MySqlCommand($"INSERT INTO nbs(productName, Qty, Price) VALUES({productName}, {qty}, {price}",Con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Base_Project_C_Sharp
+{
+    class Program
+    {
+        private static DBInteraction db = new DBInteraction();
+        static void Main(string[] args)
+        {
+            string menuOneEntry = "";
+            string wholeMenuEntry = "";
+            string menuTwoEntry = "";
+
+            while (menuOneEntry != "q")
+            {
+                Console.WriteLine("Press 1 to insert into table.");
+                Console.WriteLine("Press 2 to view table or view table filters.");
+                Console.WriteLine("Press q to exit this menu.");
+
+                menuOneEntry = Console.ReadLine();
+                Console.Clear();
+
+                if (menuOneEntry == "1")
+                {
+                    Program.createEntry();
+                }
+
+                if (menuOneEntry == "2")
+                {
+                    while (menuTwoEntry != "q")
+                    {
+                        Console.WriteLine("Press 1 to view whole sales table.");
+                        Console.WriteLine("Press 2 to view filter.");
+                        Console.WriteLine("Press q to exit this menu.");
+
+                        menuTwoEntry = Console.ReadLine();
+                        Console.Clear();
+
+                        switch (menuTwoEntry)
+                        {
+                            case "1": db.printTable("SELECT * FROM nbs"); break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void createEntry()
+        {
+            string productName = "";
+            float price = 0;
+            int qty = 0;
+
+            Console.WriteLine("Please enter product name: ");
+            productName = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Please enter product price: ");
+            price = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Please enter product quantity: ");
+            qty = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+
+            DBInteraction db = new DBInteraction();
+            db.insertIntoTable(productName, price, qty);
+
+            Console.WriteLine("Success!");
+            Console.ReadLine();
+        }
+    }
+}
